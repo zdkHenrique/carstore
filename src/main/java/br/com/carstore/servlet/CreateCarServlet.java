4ed0ebdba052a2bc9d31e3844975d0a3c071247f
HENRIQUE SENA DE MENEZES <henrique.smenezes1@senacsp.edu.br>
2024-03-20 22:06:26 -0300

    criando sistema carstore

---
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -0,0 +1,24 @@
+package br.com.carstore.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+
+        String carName = request.getParameter("carName");
+
+        System.out.println("O nome do carro é: " + carName);
+        request.getRequestDispatcher("index.html").forward(request, resp);
+
+
+
+    }
+}
